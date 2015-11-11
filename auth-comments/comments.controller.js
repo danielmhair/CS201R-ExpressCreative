741ec87c7e87c941d39cd56f88b6c0c282689d1d
Daniel Hair <danielmhair@gmail.com>
2015-11-11 01:43:15 +0000

    Added a bit more functionality

--- auth-comments/comments.controller.js
+++ auth-comments/comments.controller.js
@@ -2,6 +2,9 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('CommentsController', ['$scope', '$http', function($scope, $http) {
     $scope.comments = [];
+    $scope.name = "Mickey";
+    $scope.pass = "mickeypass";
+    $scope.comment = "Todo item";
     
     $http.get("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/comment").success(function(data) {
         data.forEach(function(item) {
@@ -10,33 +13,31 @@ myApp.controller('CommentsController', ['$scope', '$http', function($scope, $htt
     });
     
     $scope.postComment = function() {
-        var myobj = {"Name" : $("#Name").val(), 
-                     "Password" : $("#Password").val(), 
-                     "Comment" : $("#Comment").val()};
+        var myobj = {"Name"     : $scope.name, 
+                     "Password" : $scope.pass, 
+                     "Comment"  : $scope.comment };
         var jobj = JSON.stringify(myobj);
-        $http.post("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/comment", jobj, {headers: { 'Content-Type' : "application/json; charset=utf-8"}})
+        $http({
+            url: "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/credentials",
+            method: "GET",
+            params: jobj
+        })
         .then(function(res) {
-          var data = JSON.parse(res.config.data)
-          $scope.addComment(data.Name, data.Comment);
-        }, function(data, status) {
-            console.log(data);
-            console.log(status);
-        });
-    };
-    
-    $scope.authorize = function() {
-        var myobj = {"Name" : $("#Name").val(), 
-                     "Password" : $("#Password").val(), 
-                     "Comment" : $("#Comment").val()};
-        var jobj = JSON.stringify(myobj);
-        
-        $http.get("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/credentials", jobj, {headers: { 'Content-Type' : "application/json; charset=utf-8"}})
-        .then(function(res) {
-          var data = JSON.parse(res.config.data)
-          $scope.addComment(data.Name, data.Comment);
-        }, function(data, status) {
-            console.log(data);
-            console.log(status);
+            if (res.status == 200) {
+                $http.post("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/comment", jobj, {headers: { 'Content-Type' : "application/json; charset=utf-8"}})
+                .then(function(res) {
+                  console.log(res);
+                  var data = JSON.parse(res.config.data);
+                  console.log(data);
+                  $scope.addComment(data.Name, data.Comment);
+                })
+                .catch(function(data, status) {
+                    console.log(data);
+                    console.log(status);
+                });
+            }
+        }).catch(function(err) {
+            console.log(err);
         });
     };
     
