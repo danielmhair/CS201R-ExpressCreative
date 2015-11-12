75ad29482181e4096b77b9eaec2db18a239e547b
Daniel Hair <danielmhair@gmail.com>
2015-11-12 16:20:56 +0000

    Finished requirements for lab, still needs styling

--- auth-comments/comments.controller.js
+++ auth-comments/comments.controller.js
@@ -5,41 +5,73 @@ myApp.controller('CommentsController', ['$scope', '$http', function($scope, $htt
     $scope.name = "Mickey";
     $scope.pass = "mickeypass";
     $scope.comment = "Todo item";
+    $scope.loggedIn = false;
+    $scope.newUser = false;
     
-    $http.get("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/comment").success(function(data) {
-        data.forEach(function(item) {
-            $scope.addComment(item.Name, item.Comment);
-        });
-    });
-    
-    $scope.postComment = function() {
+    $scope.login = function() {
         var myobj = {"Name"     : $scope.name, 
-                     "Password" : $scope.pass, 
-                     "Comment"  : $scope.comment };
-        var jobj = JSON.stringify(myobj);
+                     "Password" : $scope.pass };
         $http({
-            url: "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/credentials",
+            url: "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007/credentials",
             method: "GET",
-            params: jobj
+            params: myobj
         })
         .then(function(res) {
-            if (res.status == 200) {
-                $http.post("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com/comment", jobj, {headers: { 'Content-Type' : "application/json; charset=utf-8"}})
-                .then(function(res) {
-                  console.log(res);
-                  var data = JSON.parse(res.config.data);
-                  console.log(data);
-                  $scope.addComment(data.Name, data.Comment);
-                })
-                .catch(function(data, status) {
-                    console.log(data);
-                    console.log(status);
+            console.log("Logged in");
+            $scope.comments = [];
+            $http.get("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007/comment?Name="+$scope.name).success(function(data) {
+                data.forEach(function(item) {
+                    $scope.addComment(item.Name, item.Comment);
                 });
-            }
+            });
+            $scope.loggedIn = true;
+            
         }).catch(function(err) {
             console.log(err);
+            console.log("Not Logged in");
+            $scope.loggedIn = false;
         });
     };
+    
+    $scope.logout = function() {
+        $scope.loggedIn = false;
+        $scope.newUser = false;
+    }
+    
+    $scope.register = function() {
+        $scope.newUser = true;
+        $scope.postComment("Add todo items and get started on a very planned life!");
+        $scope.loggedIn = true;
+    }
+    
+    $scope.checkAuthThenComment = function() {
+        
+        if ($scope.loggedIn || $scope.newUser) {
+            $scope.postComment($scope.comment);
+        } else {
+            $scope.postMessage = "Sorry, you are not logged in or have not registered";
+            console.log($scope.postMessage);
+        }
+    };
+    
+    $scope.postComment = function(comment) {
+        var myobj = {"Name"     : $scope.name, 
+                     "Password" : $scope.pass, 
+                     "Comment"  : comment };
+        
+        var jobj = JSON.stringify(myobj);
+        $http.post("http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007/comment", jobj, {headers: { 'Content-Type' : "application/json; charset=utf-8"}})
+        .then(function(res) {
+          console.log(res);
+          var data = JSON.parse(res.config.data);
+          console.log(data);
+          $scope.addComment(data.Name, data.Comment);
+        })
+        .catch(function(data, status) {
+            console.log(data);
+            console.log(status);
+        });
+    }
     
     $scope.addComment = function(addName, addComment) {
         $scope.comments.push({name:addName, comment:addComment});
--- server.js
+++ server.js
@@ -17,36 +17,48 @@ var options = {
     cert: fs.readFileSync('ssl/server.crt')
 };
 
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);
+http.createServer(app).listen(3007);
 
 app.use(bodyParser());
-app.use('/Lab6-ExpressCreative', express.static('./auth-comments', {maxAge: 60*60*1000}));
+app.use('/', express.static('./auth-comments', {maxAge: 60*60*1000}));
 
 app.get('/comment', function (req, res) {
-  // Read all of the database entries and return them in a JSON array
-  MongoClient.connect("mongodb://localhost/comments", function(err, db) {
-    if(err) throw err;
-    db.collection("comments", function(err, comments){
-      if(err) console.log(err);
-      comments.find(function(err, items){
-        items.toArray(function(err, itemArr){
-            console.log(itemArr);
-            res.writeHead(200, { "Access-Control-Allow-Origin": "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com" });
-            res.end(JSON.stringify(itemArr));
-        });
+  var userQuery = url.parse(req.url, true).query;
+   console.log(userQuery);
+   MongoClient.connect("mongodb://localhost/comments", function(err, db) {
+    if (err) throw err;
+    db.collection("comments", function(err, comments) {
+     if (err) throw err;
+     comments.find(userQuery,function(err, items) {
+      items.toArray(function(err, itemArr) {
+       res.writeHead(200, { "Access-Control-Allow-Origin": "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007" });
+       res.end(JSON.stringify(itemArr));
       });
+     });
     });
-  });
+   });
+});
+
+//POST Methods
+app.post('/comment', function (req, res) {
+    var reqObj = req.body;
+    MongoClient.connect("mongodb://localhost/comments", function(err, db) {
+        if(err) console.log(err);
+        db.collection('comments').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+            res.writeHead(200, { "Access-Control-Allow-Origin": "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007" });
+            res.end(JSON.stringify(records[0]));
+        });
+    });
 });
 
 app.get('/credentials', function(req, res) {
     console.log("getting credentials");
-    res.header("Access-Control-Allow-Origin","http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com");
-    var username = req.body.username;
-    console.log(username);
-    var password = req.body.password;
-    console.log(password);
+    console.log(req.query);
+    res.header("Access-Control-Allow-Origin","http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com:3007");
+    
+    var username = req.query.Name;
+    var password = req.query.Password;
     MongoClient.connect("mongodb://localhost/comments", function(err, db) {
         if(err) throw err;
         db.collection("comments", function(err, comments){
@@ -54,8 +66,9 @@ app.get('/credentials', function(req, res) {
             comments.find(function(err, items){
                 items.toArray(function(err, itemArr){
                         itemArr = itemArr.filter(function(item) {
-                            console.log(item);
-                            return item.Name == username && item.Password == password;
+                            //console.log(item);
+                            //We want item.Password to be defined
+                            return item.Name === username && item.Password === password;
                         });
                         console.log("itemArr");
                         console.log(itemArr);
@@ -72,17 +85,3 @@ app.get('/credentials', function(req, res) {
     });
     
 });
-
-//POST Methods
-app.post('/comment', function (req, res) {
-    var reqObj = req.body;
-    MongoClient.connect("mongodb://localhost/comments", function(err, db) {
-        if(err) console.log(err);
-        db.collection('comments').insert(reqObj,function(err, records) {
-            console.log("Record added as "+records[0]._id);
-            res.writeHead(200, { "Access-Control-Allow-Origin": "http://ec2-52-88-191-100.us-west-2.compute.amazonaws.com" });
-            res.end(JSON.stringify(records[0]));
-        });
-    });
-});
-
